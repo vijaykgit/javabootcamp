6c5cf7df359c2d3603a4c17b51beccd1a7d860af
vinodkj <vinodrajkj@gmail.com>
2019-06-26 07:41:46 +0530

    created by vinod 2 wed 26 2019 7.30 am

---
+++ MovieThetar.java
@@ -0,0 +1,34 @@
+package wavelabsbasicprograms;
+
+public class MovieTheater {
+	static int TICKETS = 50;
+
+	public void bookTickets(int n) {
+		if (n <= TICKETS) {
+			TICKETS = TICKETS - n;
+			System.out.println(" Booking" + n + "Tickets successfully......!");
+		} else {
+			System.out.println(" invlaid input <please enter below number 50>");
+		}
+	}
+
+	public void cancelTicket(int n) 
+	{
+		if(TICKETS<=50) {
+		TICKETS = TICKETS + n;
+		System.out.println(" No of" + n + " Tickets cancelled successfully");
+		}
+		else
+		{
+			System.out.println(" you dont have any Booked Tickets");
+		}
+
+	}
+
+	public void avaiableTicket() {
+		System.out.println(" only this"+"  "+TICKETS+" "+"Tickets Left");
+
+	}
+
+}
+
